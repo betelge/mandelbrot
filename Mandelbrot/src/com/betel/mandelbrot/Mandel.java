273e9c487776a1aeb5326598f18b8ac68181df14
jasminko <jasminko@d03601d8-d3e6-4e24-8c02-638dcdb6519a>
2014-09-12 01:39:06 +0000

    Implemented Mandel calculations in vertex shader Implemented double emulation in vertex shader too Removed check boxes in menu Only run layout change code if view size is non-zero Caused a memory leak

--- Mandelbrot/src/com/betel/mandelbrot/Mandel.java
+++ Mandelbrot/src/com/betel/mandelbrot/Mandel.java
@@ -1,11 +1,20 @@
 package com.betel.mandelbrot;
 
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
 import utils.ShaderLoader;
 import utils.StringLoader;
 import betel.alw3d.Alw3dModel;
 import betel.alw3d.Alw3dView;
+import betel.alw3d.renderer.CameraNode;
 import betel.alw3d.renderer.FBO;
+import betel.alw3d.renderer.Geometry;
+import betel.alw3d.renderer.GeometryNode;
 import betel.alw3d.renderer.Material;
+import betel.alw3d.renderer.Node;
 import betel.alw3d.renderer.QuadRenderPass;
 import betel.alw3d.renderer.RenderMultiPass;
 import betel.alw3d.renderer.ShaderProgram;
@@ -14,6 +23,7 @@ import betel.alw3d.renderer.Uniform;
 import betel.alw3d.renderer.passes.CheckGlErrorPass;
 import betel.alw3d.renderer.passes.ClearPass;
 import betel.alw3d.renderer.passes.RenderPass;
+import betel.alw3d.renderer.passes.SceneRenderPass;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
 import android.app.AlertDialog;
@@ -49,13 +59,17 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 	private ShaderProgram mandelShaderProg;
 	private ShaderProgram mandel64ShaderProg;
 	private ShaderProgram mandel64ExpShaderProg;
+	private ShaderProgram mandelInVertexShader;
+	private ShaderProgram mandel64InVertexShader;
 	private float scaleMandelX = 1f, scaleMandelY = 1f;
 	private double offsetMandelX = 0f, offsetMandelY = 0f;
 	private Uniform scaleMandelUniform;
 	private Uniform offsetMandelUniform;
 	private Uniform offsetFineMandelUniform;
 	
-	RenderPass finalPass;
+	private SceneRenderPass mandelVertexPass;
+	
+	private RenderPass finalPass;
 	private Material finalMaterial;
 	private ShaderProgram finalShaderProg;
 	private float scaleFinalX = 1f, scaleFinalY = 1f;
@@ -89,6 +103,8 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 		mandelShaderProg = ShaderLoader.loadShaderProgram(R.raw.mandel_v, R.raw.mandel_f);
 		mandel64ShaderProg = ShaderLoader.loadShaderProgram(R.raw.mandel64_v, R.raw.mandel64_f);
 		mandel64ExpShaderProg = ShaderLoader.loadShaderProgram(R.raw.mandel64exp_v, R.raw.mandel64exp_f);
+		mandelInVertexShader = ShaderLoader.loadShaderProgram(R.raw.mandel_in_vertex_v, R.raw.mandel_in_vertex_f);
+		mandel64InVertexShader = ShaderLoader.loadShaderProgram(R.raw.mandel64exp_in_vert_v, R.raw.mandel64exp_in_vert_f);
 		mandelMaterial = new Material(mandelShaderProg);
 		
 		maxInterationsUniform = new Uniform("MAX_ITER", 40f);
@@ -102,7 +118,6 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 		mandelMaterial.addUniform(scaleMandelUniform);
 		splitterFloatUniform = new Uniform("split", 1025f);
 		mandelMaterial.addUniform(splitterFloatUniform);
-		
 		
 
 		scaleDetector = new ScaleGestureDetector(this, new ScaleListener());
@@ -130,29 +145,41 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
+		if(id == R.id.normalMandel) {
+			mandelMaterial.setShaderProgram(mandelShaderProg);
+			mandelPass.setSilent(false);
+		}
 		if(id == R.id.emulateDoubleCheckBox) {			
-			if( item.isChecked()) {
+			/*if( item.isChecked()) {
 	            mandelMaterial.setShaderProgram(mandelShaderProg);
 	            item.setChecked(false);
 	        }
-	        else {
+	        else {*/
 	        	mandelMaterial.setShaderProgram(mandel64ShaderProg);
-	        	item.setChecked(true);
-	        }
+	        	/*item.setChecked(true);
+	        }*/
 			mandelPass.setSilent(false);
 			return true;
 		}
 		if(id == R.id.experimentalShader) {
-			if(item.isChecked()) {
+			/*if(item.isChecked()) {
 				mandelMaterial.setShaderProgram(mandel64ShaderProg);
 	        	item.setChecked(false);
 			}
-			else {
+			else {*/
 	        	mandelMaterial.setShaderProgram(mandel64ExpShaderProg);
-	        	item.setChecked(true);
-	        }
+	        	/*item.setChecked(true);
+	        }*/
 			mandelPass.setSilent(false);
 			return true;
+		}
+		if(id == R.id.mandelInVertex) {
+			mandelMaterial.setShaderProgram(mandelInVertexShader);
+			mandelPass.setSilent(false);
+		}
+		if(id == R.id.emulatedDoubleInVertex) {
+			mandelMaterial.setShaderProgram(mandel64InVertexShader);
+			mandelPass.setSilent(false);
 		}
 		if(id == R.id.chooseMaxIter) {
 			maxIterDialog = new AlertDialog.Builder(this);
@@ -281,6 +308,7 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 	@Override
 	public void onLayoutChange(View v, int left, int top, int right,
 			int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+		if(view.getHeight() == 0 || view.getWidth() == 0) return;
 		
 		// This will be done first after the view receives it's size in the layout.
 		
@@ -308,22 +336,78 @@ public class Mandel extends ActionBarActivity implements OnTouchListener, OnLayo
 		// TODO: Are we leaking video memory here when we loose the old FBO?
 		mandelFBO = new FBO(mandelTexture, view.getWidth(), view.getHeight());
 		
+		// Initialize the pixelMesh
+		Node vertexRootNode = new Node();
+		mandelVertexPass = new SceneRenderPass(vertexRootNode,
+				/*Ignored dummy camera*/ new CameraNode(asp, asp, asp, asp)/*,
+				mandelFBO*/);
+		mandelVertexPass.setOneTime(true);
+		
+		final int maxShort = 256*256-1;
+		int w = view.getWidth();
+		int h = view.getHeight();
+		int fullH = h;
+		
+		// Limit the indices to the size of short
+		if(w*h > maxShort) {
+			int r = w % maxShort;
+			h = (maxShort - r) / h;
+		}
+
+		// How many short sized pieces do we need?
+		int parts = (int)Math.ceil((float)fullH/h);
+		
+		for(int p = 0; p < parts; p++) {
+			// Are we at an end piece that needs to be smaller?
+			int trunc = 0;
+			if((p+1)*h > fullH) {
+				trunc = (p+1)*h - fullH;
+			}
+			ShortBuffer indices = ShortBuffer.allocate(w*h);
+			Geometry.Attribute posAt = new Geometry.Attribute();
+			posAt.name = "position";
+			posAt.size = 3;
+			posAt.type = Geometry.Type.FLOAT;
+			posAt.buffer = FloatBuffer.allocate(3*w*h);
+			
+			for(int i = 0; i < h-trunc; i++) {
+				for(int j = 0; j < w; j++) {
+					((FloatBuffer) posAt.buffer).put(-1 + (2*j + 1)/(float)w);
+					((FloatBuffer) posAt.buffer).put(-1 + (2*(i+p*h) + 1)/(float)fullH);
+					((FloatBuffer) posAt.buffer).put(0);
+					
+					indices.put((short)(i*w+j));
+				}
+			}
+			posAt.buffer.flip();
+			indices.flip();
+			
+			List<Geometry.Attribute> lat = new ArrayList<Geometry.Attribute>();
+			lat.add(posAt);
+			Geometry pixelMesh = new Geometry(Geometry.PrimitiveType.POINTS, indices, lat);
+			
+			GeometryNode pixelGeometryNode = new GeometryNode(pixelMesh, mandelMaterial);
+			
+			vertexRootNode.attach(pixelGeometryNode);
+		}
 
 		finalMaterial = new Material(finalShaderProg);
 		finalMaterial.addTexture("tex", mandelTexture);
 		finalMaterial.addUniform(offsetFinalUniform);
 		finalMaterial.addUniform(scaleFinalUniform);
 		
-		clearPass = new ClearPass(GLES20.GL_COLOR_BUFFER_BIT, null);
-		model.addRenderPass(clearPass);
-		mandelPass = new QuadRenderPass(mandelMaterial, mandelFBO);
-		mandelPass.setOneTime(true);
-		model.addRenderPass(mandelPass);
-		finalPass = new QuadRenderPass(finalMaterial);
-		model.addRenderPass(finalPass);
-		model.addRenderPass(new CheckGlErrorPass(true));		
-		
-		
+		synchronized (model.getRenderPasses()) {
+			model.getRenderPasses().clear();
+			clearPass = new ClearPass(GLES20.GL_COLOR_BUFFER_BIT, null);
+			model.addRenderPass(clearPass);
+			mandelPass = mandelVertexPass;//new QuadRenderPass(mandelMaterial, mandelFBO);
+			mandelVertexPass.setFbo(mandelFBO);
+			mandelPass.setOneTime(true);
+			model.addRenderPass(mandelPass);
+			finalPass = new QuadRenderPass(finalMaterial);
+			model.addRenderPass(finalPass);
+			model.addRenderPass(new CheckGlErrorPass(true));		
+		}
 	}
 
 }
